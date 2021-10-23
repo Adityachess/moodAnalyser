1608ff775479a68b7943113fe26b822c474aeab7
Adityachess <adityadone9398@gmail.com>
2021-10-24 00:58:47 +0530

    Added:To analyse happy mood of the user.

---
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.moodanalyser;
+
+public class MoodAnalyser {
+	public static String analyseMood(String message) {
+		if (message.toLowerCase().contains("sad")) {
+			return "Sad";
+		}
+		return "Happy";
+	}
+
+}
