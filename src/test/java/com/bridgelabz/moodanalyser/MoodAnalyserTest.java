e7238b7f0372265f0ac68bf6aa867a98d3b8be69
Adityachess <adityadone9398@gmail.com>
2021-10-24 01:04:33 +0530

    Added:Given null mood return happy.

---
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.moodanalyser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+
+	// Test case to analyze sad mood of the user
+	@Test
+	public void givenMessageWhenProperShouldReturnSad() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
+		String actualResult = MoodAnalyser.analyseMood();
+		Assert.assertEquals("Sad", actualResult);
+	}
+
+	// Test case to analyze happy mood of the user
+	@Test
+	public void givenMessageContainsAnyInitShouldReturnHappy() {
+		MoodAnalyser moodanalyser = new MoodAnalyser("I am in Happy Mood");
+		String actualResult = MoodAnalyser.analyseMood();
+		Assert.assertEquals("Happy", actualResult);
+	}
+
+	@Test
+	public void givenMessage_Null_ShouldReturnHappy() {
+		MoodAnalyser moodanalyser = new MoodAnalyser(null);
+		String actualResult = MoodAnalyser.analyseMood();
+		Assert.assertEquals("Happy", actualResult);
+	}
+
+}
